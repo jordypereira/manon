690b69753e5fc4dc1f8a326fb76c27bff1c4002b
Jordy Pereira <jordy.pereira@prorenata.se>
2019-03-19 17:36:43 +0100

    Fix postcss bug

--- 22/postcss.config.js
+++ 22/postcss.config.js
@@ -1,12 +1,24 @@
 const purgecss = require("@fullhuman/postcss-purgecss");
 const isDev = process.argv.indexOf("serve") !== -1;
+class TailwindExtractor {
+  static extract(content) {
+    return content.match(/[A-Za-z0-9-_:/]+/g) || [];
+  }
+}
+
 module.exports = {
   plugins: [
     require("tailwindcss")("./src/tailwind/tailwind.js"),
     isDev
       ? ""
       : purgecss({
-          content: ["./src/**/*.html", "./src/**/*.vue", "./src/**/*.less"]
+          content: ["./src/**/*.html", "./src/**/*.vue"],
+          extractors: [
+            {
+              extractor: TailwindExtractor,
+              extensions: ["html", "vue"]
+            }
+          ]
         }),
     require("autoprefixer")
   ]
