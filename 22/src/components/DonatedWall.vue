201ee88e0c76bfaeabde2a0fe3dda52df3740a88
Jordy Pereira <jordy.pereira@prorenata.se>
2019-03-22 02:00:45 +0100

    Added a star for special kitties

--- 22/src/components/DonatedWall.vue
+++ 22/src/components/DonatedWall.vue
@@ -6,12 +6,13 @@
     <div class="mx-auto flex flex-wrap justify-around">
       <a
         :href="item.url"
-        class="flex flex-col items-center"
+        class="flex flex-col items-center relative"
         v-for="item in items"
         :key="item.url"
         rel="noopener"
         target="_blank"
       >
+        <div v-if="item.type === 'original'" class="original-kitty" id="star-five"></div>
         <BasePicture
           class="flex items-center justify-center m-4 shadow-cyber-rotate-hover md:w-32 md:h-32 lg:w-48 lg:h-48"
           :imageUrl="item.url"
